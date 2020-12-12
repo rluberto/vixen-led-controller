91aea8743d71952549010c56d65369d5732d879f
= <=>
2020-12-11 20:08:30 -0500

    create functions

--- app.ino
+++ app.ino
@@ -1,8 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 
-int data_pin = 6; 
+int data_pin = 6; //Define the data pin that will be used on the arduino
 int led_count = 150; //Define the number of leds on the strip
 int led_brightness = 100; //Set the brightness of the strip (max: 255)
+
+int channel_array[led_count][3];
+
 
 //Define the led strip
 Adafruit_NeoPixel strip(led_count, data_pin, NEO_GRBW + NEO_KHZ800);
@@ -17,6 +20,17 @@ void setup(){
 
 void loop(){
 	if(Serial.available() >= led_count){
-
+        read_serial();
+        write_serial();
     }
+}
+
+//This function will read all of the values from the serial connection to vixen lights and put the valued into the array
+void read_serial(){
+
+}
+
+//This function will take the values from the array and write them to the neopixel strip
+void write_strip(){
+
 }
